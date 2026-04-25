f54969affe94d9accc7d78c159df3018c2cc3c2b
fxdil21x <fadilfadu90@gmail.com>
2026-04-25 14:25:48 +0530

    env REMOVED

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -3,12 +3,12 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  apiKey: "AIzaSyCvfzVvhv3lDC4rzJvgbMXJK-trh65Eg1M",
+  authDomain: "teaching-hours-ae6a1.firebaseapp.com",
+  projectId: "teaching-hours-ae6a1",
+  storageBucket: "teaching-hours-ae6a1.firebasestorage.app",
+  messagingSenderId: "239533580131",
+  appId: "1:239533580131:web:19841566c3d777a7eafd56",
 };
 
 const app = initializeApp(firebaseConfig);
